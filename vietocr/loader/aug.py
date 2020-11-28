2291c20e88be39e2da6058ffd1aeb6dfbca25f0e
Bang Vo <vqbang1996@gmail.com>
2020-11-28 23:12:08 +0700

    debug

--- vietocr/loader/aug.py
+++ vietocr/loader/aug.py
@@ -8,15 +8,13 @@ class ImgAugTransform:
   def __init__(self):
     sometimes = lambda aug: iaa.Sometimes(0.5, aug)
 
-    self.aug = iaa.SomeOf(1, 
+    self.aug = iaa.SomeOf(3, 
         [
-        #sometimes(iaa.GaussianBlur(sigma=(0, 1.0))),
-        #sometimes(iaa.MotionBlur(k=3)),
-        sometimes(iaa.GammaContrast((0.5, 2.0), per_channel=True)),
-        sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.03))),
-        sometimes(iaa.Invert(0.25, per_channel=0.5)),
-        sometimes(iaa.Rotate((-20, 20))),
-        sometimes(iaa.LogContrast(gain=(0.6, 1.4), per_channel=True))
+        sometimes(iaa.AddToHueAndSaturation((-50, 50), per_channel=True) ),
+        sometimes(iaa.SigmoidContrast(gain=(3, 10), cutoff=(0.4, 0.6), per_channel=True)),
+        #sometimes(iaa.PiecewiseAffine(scale=(0.01, 0.02))),
+        sometimes(iaa.Invert(0.7, per_channel=0.8)),
+        sometimes(iaa.Rotate((-20, 20)))
     ])
       
   def __call__(self, img):
@@ -24,4 +22,3 @@ class ImgAugTransform:
     img = self.aug.augment_image(img)
     img = Image.fromarray(img)
     return img
-
